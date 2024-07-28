20ee3997d2ce1f62ccfe71f587b0fce4619b92ee
Arand2411 <enzoquesada2014@gmail.com>
2024-07-27 19:16:50 -0600

    ADD Register/Login
    Se completa el apartado de register y login de usuario
    
    Co-Authored-By: Jaime <113743405+jaime2804@users.noreply.github.com>

--- Proyecto_Web/Proyecto_Web/Program.cs
+++ Proyecto_Web/Proyecto_Web/Program.cs
@@ -1,7 +1,16 @@
+using Proyecto_Web.Models;
+using Proyecto_Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IUsuarioModel, UsuarioModel>();
+builder.Services.AddSingleton<IComunModel, ComunModel>();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -9,7 +18,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+
     app.UseHsts();
 }
 
@@ -20,8 +29,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseSession();
+
 app.MapControllerRoute(
     name: "default",
+
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
