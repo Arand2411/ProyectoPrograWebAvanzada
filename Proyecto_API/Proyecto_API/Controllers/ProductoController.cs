2b042a932b5a1d9136ee626055a999888184eae4
AngelUFI <160547990+AngelUFI@users.noreply.github.com>
2024-07-29 21:09:41 -0600

    Registro y lectura productos

---
+++ Proyecto_API/Proyecto_API/Controllers/ProductoController.cs
@@ -0,0 +1,69 @@
+﻿using Proyecto_API.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Data;
+using static Proyecto_API.Entities.Producto;
+using Dapper;
+
+namespace Proyecto_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController(IConfiguration iConfiguration) : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("RegistrarProducto")]
+        public async Task<IActionResult> RegistrarProducto(Producto ent)
+        {
+            ProductoRespuesta resp = new ProductoRespuesta();
+
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var result = await context.ExecuteAsync("RegistrarProducto", new { ent.Descripcion, ent.Precio, ent.Cantidad, ent.Ruta_Imagen, ent.Ruta_Video, ent.Estado, ent.IdCategoria }, commandType: CommandType.StoredProcedure);
+
+                if (result > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "Producto Guardado Con exito";
+                    resp.Contenido = true;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "No se pudo Guardar el Producto";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
+
+        [HttpGet]
+        [Route("ConsultarProductos")]
+        public async Task<IActionResult> ConsultarProductos()
+        {
+            ProductoRespuesta resp = new ProductoRespuesta();
+            using (var context = new SqlConnection(iConfiguration.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var result = await context.QueryAsync<Producto>("ConsultarProductos", new { }, commandType: CommandType.StoredProcedure);
+
+                if (result.Count() > 0)
+                {
+                    resp.Codigo = 1;
+                    resp.Mensaje = "Exito!!!";
+                    resp.Contenido = result;
+                    return Ok(resp);
+                }
+                else
+                {
+                    resp.Codigo = 0;
+                    resp.Mensaje = "No hay Productos";
+                    resp.Contenido = false;
+                    return Ok(resp);
+                }
+            }
+        }
+    }
+}
---
+++ Proyecto_Web/Proyecto_Web/Services/IProductoModel.cs
@@ -0,0 +1,15 @@
+﻿using Proyecto_Web.Entities;
+
+
+using Proyecto_Web.Entities;
+
+namespace Proyecto_Web.Services
+{
+    public interface IProductoModel
+    {
+        Respuesta RegistrarProducto(Producto ent);
+
+        Respuesta ConsultarProductos();
+
+    }
+}
