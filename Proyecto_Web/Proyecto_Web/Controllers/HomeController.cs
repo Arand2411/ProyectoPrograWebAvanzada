b7a31edcb6a21065885c56bb91d9dcd647adf4fd
AngelUFI <160547990+AngelUFI@users.noreply.github.com>
2024-07-29 22:14:46 -0600

    Revert "REGISTRO Y LECTURA DE PRODUCTOS"
    This reverts commit

--- Proyecto_Web/Proyecto_Web/Controllers/HomeController.cs
+++ Proyecto_Web/Proyecto_Web/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using Proyecto_Web.Entities;
 using Proyecto_Web.Models;
+using Proyecto_Web.Services;
 using System.Diagnostics;
+using System.Text.Json;
 
 namespace Proyecto_Web.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController(IProductoModel iProductoModel) : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public IActionResult Index()
         {
-            return View();
+            var resp = iProductoModel.ConsultarProductos();
+
+            if (resp.Codigo == 1)
+            {
+                var datos = JsonSerializer.Deserialize<List<Producto>>((JsonElement)resp.Contenido!);
+                return View(datos!.Where(x => x.IdProducto != HttpContext.Session.GetInt32("IDPRODUCTO")).ToList());
+            }
+
+            return View(new List<Producto>());
         }
 
         public IActionResult Privacy()
